81a75082a7589fbaac307f106ab9f3498bd32861
jordan <jordan@scenic-shop.com>
2014-01-24 00:21:35 -0500

    Resultant Force: killed dead code

--- script/resultant_force.js
+++ script/resultant_force.js
@@ -8,7 +8,6 @@ function getResultantForce() {
 	if (R.angle === "0") {
 		$("#r_force").val(Round(R.load * 2, 1));
 	} else {
-	//	temp = ((Math.sin($("#angle").val()*Math.PI / 180)) / (Math.sin(($("#angle").val()/2) * Math.PI / 180))) * $("#load").val();
 		temp = 2 * R.load * Math.cos(degtorad(R.angle/2));
 	temp 
 		$("#r_force").val(Round(temp, 1));
