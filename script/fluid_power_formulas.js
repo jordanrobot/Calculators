12844cc98305d3290c72c7bd2e81418854f3efbd
jordan <jordan@scenic-shop.com>
2014-02-19 23:05:14 -0500

    Piston Force: updated for new get() function
    * added an object
    * cleaned up code
    * deleted "name" properties in html
    * renamed "id" properties as needed
    * added rational step values to input fields

--- script/fluid_power_formulas.js
+++ script/fluid_power_formulas.js
@@ -1,53 +1,72 @@
-	var cyl = [];
-
-	function AreaOfaCircle(diameter) {
-		return (Math.pow((diameter / 2), 2) * Math.PI);
-		}
+var c= {
+	"psi": 					"",
+	"rod": 					"",
+	"bore": 				"",
+	"stroke": 				"",
+	"temperature":			"",
+	"atm": 					"",
+	"pushA": 				"",
+	"pullA": 				"",
+	"flow": 				"",
+	"psi_drop": 			"",
+	"cv_req": 				"",
+	"flow_constant": 		"",
+	"pushF":	 			"",
+	"pullF": 				"",
+	"pushDisp": 			"",
+	"pullDisp": 			"",
+	"in_per_sec_push_val": 	"",
+	"in_per_sec_pull_val": 	"",
+	"pushTime": 			"",
+	"pullTime": 			"",
+	"slider": 				""
+};
 	
-	function Calculate_cyl() {
+function Calculate_cyl() {
 		
-		cyl.psi = $('#psi').val() * 1;
-		cyl.rod  = $('#rod').val() * 1;
-		cyl.bore = $('#bore').val() * 1;
-		cyl.stroke = $('#stroke').val() * 1;
-		cyl.temperature = $('#temperature').val() * 1;
-		cyl.atm = $('#atm').val() * 1;		
-		push_area = AreaOfaCircle(cyl.bore);
-		pull_area = AreaOfaCircle(cyl.bore) - AreaOfaCircle(cyl.rod);
+	get(c);
 
-		cyl.flow = $('#flow').val() * 1;
-		cyl.drop = $('#psi_drop').val() * 1;
-//		engineer's toolbox
-		cyl.cv_req = ((cyl.flow * 60) / 1360) * Math.sqrt ((1 * (460 + cyl.temperature)) / (cyl.drop * (cyl.psi + cyl.atm - cyl.drop)));
+	// Areas
+	c.pushA = AreaOfaCircle(c.bore);
+	c.pullA = AreaOfaCircle(c.bore) - AreaOfaCircle(c.rod);
 
+	//	required cv value for air - engineer's toolbox
+	c.cv_req = ((c.flow * 60) / 1360) * Math.sqrt ((1 * (460 + c.temperature)) / (c.psi_drop * (c.psi + c.atm - c.psi_drop)));
 
-	if ($("#slider").val() == "SCFM") {
-		flow_constant = 1728;
-		$('#cv_req').val(Round(cyl.cv_req, 4));
+	// set flow constant for either air (SCFM) or fluid (GPM)
+	if (c.slider === "SCFM") {
+		c.flow_constant = 1728;
+		$('#cv_req').val(Round(c.cv_req, 4));
 	}
 	else {
-		flow_constant = 231;
+		c.flow_constant = 231;
 		$('#cv_req').val("NA");
 		
 	}
-			
-		cyl.push = cyl.psi * push_area;
-		cyl.pull = cyl.psi * pull_area;
-	
-		$('#pushForce').val(Round(cyl.push, 1));
-		$('#pullForce').val(Round(cyl.pull, 1));
-		$('#push_displacement').val(Round(push_area*cyl.stroke, 2));
-		$('#pull_displacement').val(Round(pull_area*cyl.stroke, 2));
 
+	// Forces
+	c.pushF = c.psi * c.pushA;
+	c.pullF = c.psi * c.pullA;
+	$('#pushF').val(Round(c.pushF, 1));
+	$('#pullF').val(Round(c.pullF, 1));
 
-		in_per_sec_push_val = (flow_constant*cyl.flow)/(push_area*60);
-		in_per_sec_pull_val = (flow_constant*cyl.flow)/(pull_area*60);
-		
-		$('#in_per_sec_push').val(Round(in_per_sec_push_val, 3));
-		$('#in_per_sec_pull').val(Round(in_per_sec_pull_val, 3));
+	// Displacement
+	c.pushDisp = c.pushA * c.stroke;
+	c.pullDisp = c.pullA * c.stroke;
+	$('#pushDisp').val(Round(c.pushDisp, 2));
+	$('#pullDisp').val(Round(c.pullDisp, 2));
 
-		$('#push_time').val(Round(cyl.stroke / in_per_sec_push_val, 1));
-		$('#pull_time').val(Round(cyl.stroke / in_per_sec_pull_val, 1));
+	// Velocity
+	c.in_per_sec_push_val = (c.flow_constant * c.flow) / (c.pushA * 60);
+	c.in_per_sec_pull_val = (c.flow_constant * c.flow) / (c.pullA * 60);
+	$('#in_per_sec_push').val(Round(c.in_per_sec_push_val, 3));
+	$('#in_per_sec_pull').val(Round(c.in_per_sec_pull_val, 3));
+
+	// Time
+	c.pushTime = c.stroke / c.in_per_sec_push_val;
+	c.pullTime = c.stroke / c.in_per_sec_pull_val;
+	$('#pushTime').val(Round(c.pushTime, 1));
+	$('#pullTime').val(Round(c.pullTime, 1));
 
 	}
 
